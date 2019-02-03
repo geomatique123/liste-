fbecad9c8a7d1054b0719ca4e2eebd8ddb54c7e6
Amine <47054375+amineamh123@users.noreply.github.com>
2019-02-03 22:33:47 +0100

    liste1

---
+++ liste1.py
@@ -0,0 +1,12 @@
+liste2=[]
+x=int(input("donner un nombre: "))
+liste3=list(range(1,x+1))
+for i in liste3:
+            if x%i==0:
+                liste2.append(i)
+print(liste2)
+
+
+
+   
+        
